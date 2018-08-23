4eaf10685c8cd35da97d85f466ba9f5fa1ea97b4
Kevin Martinez <kmartineze@ucenfotec.ac.cr.com>
2018-08-22 20:41:27 -0600

    Add create function

--- src/controllers/recipes.controller.js
+++ src/controllers/recipes.controller.js
@@ -1,40 +1,41 @@
+const mongoose = require("mongoose");
+const Favorites = require("../models/recipe.model.js");
 
-const mongoose = require('mongoose');
-const Favorites = require('../models/recipe.model.js');
-
-
-  exports.findAll = (req, res) => {
-    Favorites.find().then((recipes) => {
-      res.send(recipes)
-    }).catch((err) => {
+exports.findAll = (req, res) => {
+  Favorites.find()
+    .then(recipes => {
+      res.send(recipes);
+    })
+    .catch(err => {
       res.status(500).send({
         message: err.message
       });
-    })
-  };
+    });
+};
 
 exports.create = (req, res) => {
-  if(!req.body.content) {
-      return res.status(400).send({
-          message: "Note content can not be empty"
-      });
+  if (!req.body.content) {
+    return res.status(400).send({
+      message: "Note content can not be empty"
+    });
   }
 
   const favorite = new Favorite({
-      title: req.body.title || "Untitled recipe", 
-      content: req.body.content
+    title: req.body.title || "Untitled recipe",
+    content: req.body.content
   });
 
   Favorites.save()
-  .then(data => {
+    .then(data => {
       res.send(data);
-  }).catch(err => {
+    })
+    .catch(err => {
       res.status(500).send({
-          message: err.message || "Some error occurred while saving the recipe."
+        message: err.message || "Some error occurred while saving the recipe."
       });
-  });
+    });
 };
 
-  exports.homePage = (req, res) => {
-    res.send('Im here');
-  };
+exports.homePage = (req, res) => {
+  res.send("Im here");
+};
